66b1eef48d30598f7ab50c26383e3f3ce3285dbd
webwithsumit <sumit2410kushwaha@gmail.com>
2024-10-12 22:42:37 +0530

    postModel

---
+++ models/postModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    caption: {
+        type: String,
+        default: ''
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
